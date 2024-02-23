a44c2cc230a524f17de42d88e272e67bd7de85aa
unknown <smartkelvin66@gmail.com>
2024-02-23 05:28:55 +0100

    stepzen

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -2,7 +2,7 @@ import { Slot, Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    // <Slot/>
+    // <Slot/> behind the screen, the stack is rendering the slot
     <Stack>
       <Stack.Screen name="index" options={{ title: "Exercises" }} />
     </Stack>
